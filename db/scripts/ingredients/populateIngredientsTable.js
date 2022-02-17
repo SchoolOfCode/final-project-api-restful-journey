e41a7e252e8b190e586d11bbf8ea9f3846588763
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-17 15:32:10 +0000

    ingredients data added

--- db/scripts/ingredients/populateIngredientsTable.js
+++ db/scripts/ingredients/populateIngredientsTable.js
@@ -1,5 +1,5 @@
 import db from "../../connection.js";
-import ingredients from ' ';
+import {ingredients} from '../../../libs/ingredientsData.js';
 
 async function populateIngredientsTable() {
   for (let i = 0; i < ingredients.length; i++) {
@@ -10,16 +10,18 @@ async function populateIngredientsTable() {
     const months = ingredients[i].months;
     const nutrition = ingredients[i].nutrition;
     const fact = ingredients[i].fact;
+
     const response = await db.query(
         `INSERT INTO ingredients (name, imgUrl, isFruit, season, months, nutrition, fact) VALUES ($1, $2, $3, $4, $5, $6, $7);`,
         [name, imgUrl , isFruit, season, months, nutrition, fact]
       );
       
-      console.log("ingredients table is populated!", response);
-  }
+    console.log("ingredients table is populated!", response);
+    }
+    db.end();
 }
 populateIngredientsTable();
 
-db.end();
+
 
 
