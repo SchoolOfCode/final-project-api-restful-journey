e8e940019e15c5ae356a03a1ef66ff770a52ec26
ajp64 <antonyjparnell@gmail.com>
2022-02-23 10:31:02 +0000

    Adding more data to ingredients table

--- libs/ingredientsData.js
+++ libs/ingredientsData.js
@@ -1,62 +1,215 @@
 export const ingredients = [
-    {
-      id: 1,
-      name: 'Carrot',
-      imgUrl: 'http://eat-seasonal.co.uk/images/food/carrot.jpg',
-      isFruit: false,
-      season: ['spring', 'summer', 'autumn', 'winter'],
-      months: [1, 2, 3, 4, 7, 8, 9, 10, 11, 12],
-      nutrition:'Carrots are loaded with Vitamin A: One cup provides over 400 percent of your daily needs. (Vitamin A is important for a lot of things, including vision, immune function and reproduction.) They are also good sources of potassium, vitamin K and vitamin E, and are, of course, a great source of fiber. Cooking carrots makes the beta carotene (which the body turns into vitamin A) more bio-available.',
-      fact: "The carrot crop is Britain's major root vegetable, producing over 700,000 tonnes of carrots each year from 9000 hectares.",
-    },
-    {
-      id: 2,
-      name: 'Leeks',
-      imgUrl: 'http://eat-seasonal.co.uk/images/food/leek.jpg',
-      isFruit: false,
-      season: ['autumn', 'winter'],
-      months: [1, 2, 3, 8, 9, 10, 11, 12],
-      nutrition: 'Leeks are very high in Vitamins A and K and are good sources of Vitamin B-6, folate, iron, calcium and manganese. They are also high in fiber and low in calories.',
-      fact: 'Leeks are available nearly all-year-round, starting in late June and ending late April or early May, depending on the weather and development of bolting, with peaks in autumn and spring.',
-    },
-    {
-      id: 3,
-      name: 'Pumpkin',
-      imgUrl:'https://images.freeimages.com/images/small-previews/234/mini-pumpkins-1385371.jpg',
-      isFruit: false,
-      season: ['autumn', 'winter'],
-      months: [8, 9, 10, 11, 12],
-      nutrition: 'In addition to beta carotene, pumpkins offer vitamin C, vitamin E, iron, and folate -- all of which strengthen your immune system. More pumpkin in your diet can help your immune cells work better to ward off germs and speed healing when you get a wound.',
-      fact: "Britain is home to Europe's largest pumpkin grower, in Lincolnshire, producing a whopping 2 million pumpkins per year.",
-    },
-    {
-      id: 4,
-      name: 'Apple',
-      imgUrl:'https://images.freeimages.com/images/small-previews/26a/apples-1325879.jpg',
-      isFruit: true,
-      season: ['autumn', 'winter'],
-      months: [1,2, 10, 11, 12],
-      nutrition: 'A medium apple provides about four grams of fiber, and about 14 percent of the daily recommended amount of Vitamin C.  They are especially high in phytonutrients like polyphenols and anthocyanins that act as antioxidants. The potential benefits include blood sugar regulation and lung support',
-      fact: 'There are nearly 8,000 different types of apples around the world with 2,000 varieties in the UK alone.',
-    },
-    {
-      id: 5,
-      name: 'Strawberries',
-      imgUrl:'https://images.freeimages.com/images/small-previews/c22/strawberry-1324897.jpg',
-      isFruit: true,
-      season: ['spring', 'summer'],
-      months: [5,6,7,8,9],
-      nutrition: "They're an excellent source of vitamin C and manganese and also contain decent amounts of folate (vitamin B9) and potassium.",
-      fact: 'There are more than 30 varieties grown in the UK and just like apples they all have their own taste, shape and even colour to differentiate themselves.',
-    },
-    {
-      id: 6,
-      name: 'Raspberries',
-      imgUrl:'https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
-      isFruit: true,
-      season: ['summer'],
-      months: [6,7,8,9],
-      nutrition: 'One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.',
-      fact: 'The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.',
-    }
-  ];
+  {
+    id: 1,
+    name: "Carrot",
+    imgUrl: "http://eat-seasonal.co.uk/images/food/carrot.jpg",
+    isFruit: false,
+    season: ["spring", "summer", "autumn", "winter"],
+    months: [1, 2, 3, 4, 7, 8, 9, 10, 11, 12],
+    nutrition:
+      "Carrots are loaded with Vitamin A: One cup provides over 400 percent of your daily needs. (Vitamin A is important for a lot of things, including vision, immune function and reproduction.) They are also good sources of potassium, vitamin K and vitamin E, and are, of course, a great source of fiber. Cooking carrots makes the beta carotene (which the body turns into vitamin A) more bio-available.",
+    fact: "The carrot crop is Britain's major root vegetable, producing over 700,000 tonnes of carrots each year from 9000 hectares.",
+  },
+  {
+    id: 2,
+    name: "Leeks",
+    imgUrl: "http://eat-seasonal.co.uk/images/food/leek.jpg",
+    isFruit: false,
+    season: ["autumn", "winter"],
+    months: [1, 2, 3, 8, 9, 10, 11, 12],
+    nutrition:
+      "Leeks are very high in Vitamins A and K and are good sources of Vitamin B-6, folate, iron, calcium and manganese. They are also high in fiber and low in calories.",
+    fact: "Leeks are available nearly all-year-round, starting in late June and ending late April or early May, depending on the weather and development of bolting, with peaks in autumn and spring.",
+  },
+  {
+    id: 3,
+    name: "Pumpkin",
+    imgUrl:
+      "https://images.freeimages.com/images/small-previews/234/mini-pumpkins-1385371.jpg",
+    isFruit: false,
+    season: ["autumn", "winter"],
+    months: [8, 9, 10, 11, 12],
+    nutrition:
+      "In addition to beta carotene, pumpkins offer vitamin C, vitamin E, iron, and folate -- all of which strengthen your immune system. More pumpkin in your diet can help your immune cells work better to ward off germs and speed healing when you get a wound.",
+    fact: "Britain is home to Europe's largest pumpkin grower, in Lincolnshire, producing a whopping 2 million pumpkins per year.",
+  },
+  {
+    id: 4,
+    name: "Apple",
+    imgUrl:
+      "https://images.freeimages.com/images/small-previews/26a/apples-1325879.jpg",
+    isFruit: true,
+    season: ["autumn", "winter"],
+    months: [1, 2, 10, 11, 12],
+    nutrition:
+      "A medium apple provides about four grams of fiber, and about 14 percent of the daily recommended amount of Vitamin C.  They are especially high in phytonutrients like polyphenols and anthocyanins that act as antioxidants. The potential benefits include blood sugar regulation and lung support",
+    fact: "There are nearly 8,000 different types of apples around the world with 2,000 varieties in the UK alone.",
+  },
+  {
+    id: 5,
+    name: "Strawberries",
+    imgUrl:
+      "https://images.freeimages.com/images/small-previews/c22/strawberry-1324897.jpg",
+    isFruit: true,
+    season: ["spring", "summer"],
+    months: [5, 6, 7, 8, 9],
+    nutrition:
+      "They're an excellent source of vitamin C and manganese and also contain decent amounts of folate (vitamin B9) and potassium.",
+    fact: "There are more than 30 varieties grown in the UK and just like apples they all have their own taste, shape and even colour to differentiate themselves.",
+  },
+  {
+    id: 6,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 7,
+    name: "Cabbages",
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Chou_1.jpg",
+    isFruit: false,
+    season: ["winter"],
+    months: [11, 0, 1, 2],
+    nutrition:
+      "In a 100 gram reference amount, raw cabbage is a rich source of vitamin C and vitamin K, containing 44% and 72%, respectively, of the Daily Value.",
+    fact: "Russia has the highest annual per capita consumption of cabbage, at 20 kg (44 lb)",
+  },
+  {
+    id: 8,
+    name: "Brussels sprout",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b6/Chou_de_bruxelles_coupe.jpg",
+    isFruit: false,
+    season: ["winter"],
+    months: [11, 0, 1, 2],
+    nutrition:
+      "Brussels sprouts are low in calories but high in many nutrients, especially fiber, vitamin K and vitamin C.",
+    fact: "In Continental Europe, the largest producers of Brussels Sprout are the Netherlands, at 82,000 metric tons, and Germany, at 10,000 tons. The United Kingdom has production comparable to that of the Netherlands, but its crop is generally not exported.",
+  },
+  {
+    id: 9,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 10,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 11,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 12,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 13,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 14,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 15,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 16,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 17,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+  {
+    id: 18,
+    name: "Raspberries",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498668629810-d79dcafa7617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+    isFruit: true,
+    season: ["summer"],
+    months: [6, 7, 8, 9],
+    nutrition:
+      "One cup of raspberries provides over 50% of the minimum daily target for vitamin C, which supports immunity and skin health and helps produce collagen. Raspberries also contain manganese and vitamin K, which both play a role in bone health. And they supply smaller amounts of vitamin E, B vitamins, magnesium, copper, iron, and potassium.",
+    fact: "The leading raspberry varieties grown in the UK have the ability to flower and fruit over a long period of up to five months, while traditional types typically produce fruit for just a four to six week period.",
+  },
+];
