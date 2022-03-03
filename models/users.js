8f0c5d181e88927498e9b74137acb837811abbe5
ajp64 <antonyjparnell@gmail.com>
2022-03-03 12:48:10 +0000

    updated post user request, succesfully working only for unique emails
    co-authored-by Luis luiscvrodrigues@gmail.com

--- models/users.js
+++ models/users.js
@@ -5,17 +5,9 @@ export async function getAllUsers() {
   return result.rows;
 }
 
-// export async function addUser(username, email, favourites) {
-//   const result = await db.query(
-//     `INSERT INTO users (username, email, favourites) VALUES ($1, $2, $3) RETURNING username, email, favourites;`,
-//     [username, email, favourites]
-//   );
-//   return result.rows;
-// }
-
 export async function addUser(username, email, favourites) {
   const result = await db.query(
-    `INSERT INTO users (username, email, favourites) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT $2 FROM table WHERE email=$2) RETURNING username, email, favourites;`,
+    `INSERT INTO users (username, email, favourites) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT $2 FROM users WHERE email=$2) RETURNING username, email, favourites;`,
     [username, email, favourites]
   );
   return result.rows;
