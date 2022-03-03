32b19ecdb3b5c02c00cf1afb4954a58c0e90aec7
ajp64 <antonyjparnell@gmail.com>
2022-03-03 12:33:16 +0000

    added model and route for adding user
    co-authored-by Luis luiscvrodrigues@gmail.com

--- models/users.js
+++ models/users.js
@@ -1,6 +1,23 @@
+import { user } from "pg/lib/defaults";
 import db from "../db/connection.js";
 
 export async function getAllUsers() {
   const result = await db.query(`SELECT * FROM users;`);
+  return result.rows;
+}
+
+// export async function addUser(username, email, favourites) {
+//   const result = await db.query(
+//     `INSERT INTO users (username, email, favourites) VALUES ($1, $2, $3) RETURNING username, email, favourites;`,
+//     [username, email, favourites]
+//   );
+//   return result.rows;
+// }
+
+export async function addUser(username, email, favourites) {
+  const result = await db.query(
+    `INSERT INTO users (username, email, favourites) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT $2 FROM table WHERE email=$2) RETURNING username, email, favourites;`,
+    [username, email, favourites]
+  );
   return result.rows;
 }
