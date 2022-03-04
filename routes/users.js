9ec5c1a2a3f6e57ab5b9c1684efc891469e4210a
ajp64 <antonyjparnell@gmail.com>
2022-03-04 10:13:19 +0000

    added routes/models for list add , list delete, user list get
    co-authored-by Luis luiscvrodrigues@gmail.com

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getAllUsers, addUser, addListItem } from "../models/users.js";
+import {
+  getAllUsers,
+  addUser,
+  addListItem,
+  deleteListItem,
+  getAllUsersItems,
+} from "../models/users.js";
 
 const router = express.Router();
 
@@ -14,21 +20,38 @@ router.get("/", async function (req, res, next) {
 });
 
 router.post("/", async function (req, res, next) {
-  const { username, email, favourites } = req.body;
-  const newUser = await addUser(username, email, favourites);
+  const { username, email, favourites, list } = req.body; // add empty array to req body on front end?
+  const newUser = await addUser(username, email, favourites, list);
   res.json({ success: true, payload: newUser });
 });
 
 router.post("/add", async function (req, res, next) {
-  console.log('body', req.body)
-  const email = req.query.email;
-  const item = req.body.item;
-  
+  console.log("body", req.body);
+  const { email, item } = req.body;
+
   const shoppingList = await addListItem(email, item);
 
   res.json({
     success: true,
     payload: shoppingList,
+  });
+});
+
+router.delete("/delete", async function (req, res, next) {
+  const { email, item } = req.body;
+  const deletedItem = await deleteListItem(email, item);
+  res.json({
+    success: true,
+    payload: deletedItem,
+  });
+});
+
+router.get("/list", async function (req, res, next) {
+  const email = req.body.email;
+  const list = await getAllUsersItems(email);
+  res.json({
+    success: true,
+    payload: list,
   });
 });
 
